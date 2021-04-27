dcae56d037c96cf1fbc387079cf96cecb4b99017
yambo <yambo1992@outlook.com>
2021-04-27 23:55:43 +0800

    转义字符

---
+++ src/go_code/chapter01/escapedChar/main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt" // fmt包提供格式化
+func main() {
+	// 转义字符
+	fmt.Println("tom\t你好")
+	fmt.Println("tom\n你好")
+	fmt.Println("C:\\Users\\hello")
+	fmt.Println("你是那个\r天神")
+
+	fmt.Println("姓名\t年龄\t籍贯\t住址\nJohn\t20\t河南\t北京")
+}
